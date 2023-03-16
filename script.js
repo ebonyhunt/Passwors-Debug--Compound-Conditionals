51a060ce221268b1b7dfeb12202fa8a9cb43a40b
ebonyhunt <107260057+ebonyhunt@users.noreply.github.com>
2023-03-16 17:28:16 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+let correctUsername = "Rose";
+let correctPassword = "Chloris-Aphrodite";
+
+$("button").click(function() {
+    let username = $(".username").val();
+    let password = $(".password").val(); 
+});
+
